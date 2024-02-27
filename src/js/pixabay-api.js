993ba01f28adf8e3a368af2daf3d91612e4145a6
YK9968 <robka4an@gmail.com>
2024-02-28 01:32:19 +0200

    update HW

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
-import { inputEL, loadMoreImgBtn } from '../main';
-import { createMarkup } from './render-functions';
 
+const limit = 15;
+export const totalPages = Math.ceil(100 / limit);
+
+import { loadPageImg, curentSearch } from '../main';
 export async function getPhotos() {
   const baseUrl = 'https://pixabay.com/api/';
   axios.defaults.baseURL = baseUrl;
-  const limit = 15;
-  let loadPageImg = 1;
-  console.log(loadPageImg);
   const key = '42471453-a4a004408e33f852748a0909e';
-  const query = inputEL.value;
   const typeImg = 'photo';
   const orientationImg = 'horizontal';
   const resultSearch = true;
+  console.log(loadPageImg);
 
   try {
     const response = await axios.get(baseUrl, {
@@ -20,21 +19,12 @@ export async function getPhotos() {
         page: loadPageImg,
         per_page: limit,
         key: key,
-        q: query,
+        q: curentSearch,
         image_type: typeImg,
         orientation: orientationImg,
         safesearch: resultSearch,
       },
     });
-
-    loadMoreImgBtn.addEventListener('click', updateImgList);
-
-    function updateImgList() {
-      console.log(response.data.hits);
-      loadPageImg += 1;
-      createMarkup(response.data.hits);
-    }
-    console.log(response.data.hits);
 
     return response.data;
   } catch (error) {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { getPhotos } from './js/pixabay-api';
+import { getPhotos, totalPages } from './js/pixabay-api';
 import { createMarkup } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
@@ -6,24 +6,32 @@ import cross from './img/error.svg';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// ================================================================ ^ import ^ ======================
 export const galleryListEl = document.querySelector('.gallery-list');
 export const inputEL = document.querySelector('input');
 const formSearchImg = document.querySelector('form');
 export const loadMoreImgBtn = document.querySelector('.load-more-img');
 export const loaderEl = document.querySelector('.form-container div');
+export let loadPageImg = 1;
+export let curentSearch;
+
+// ================================================================= ^ var ^ =========================
 loadMoreImgBtn.classList.add('hidden');
 
 formSearchImg.addEventListener('submit', handleSearchImg);
 
 function handleSearchImg(event) {
   event.preventDefault();
-
+  curentSearch = inputEL.value;
   galleryListEl.innerHTML = '';
 
   if (inputEL.value.trim() === '') {
+    loadMoreImgBtn.classList.add('hidden');
     return;
   }
   loaderEl.classList.add('loader');
+
+  // ================================================================= ^ button check ^ =====================
   getPhotos().then(data => {
     if (data.total === 0) {
       iziToast.error({
@@ -38,15 +46,43 @@ function handleSearchImg(event) {
         maxWidth: 380,
         theme: 'dark',
       });
+      loaderEl.classList.remove('loader');
+      loadMoreImgBtn.classList.add('hidden');
+      return;
     }
-
+    // ======================================================================= ^ pixabay.API ^ =============
     createMarkup(data.hits);
+    // ======================================================================= ^ markup ^ ===================
+    loadMoreImgBtn.addEventListener('click', addMoreImg);
+
+    function addMoreImg() {
+      if (loadPageImg > totalPages) {
+        loadMoreImgBtn.classList.add('hidden');
+        return iziToast.error({
+          position: 'topRight',
+          message: "We're sorry, but you've reached the end of search results.",
+          iconUrl: cross,
+          messageColor: '#ffffff',
+          backgroundColor: '#EF4040',
+
+          messageSize: 16,
+          layout: 2,
+          maxWidth: 380,
+          theme: 'dark',
+        });
+      }
+      loadPageImg += 1;
+      getPhotos().then(data => {
+        createMarkup(data.hits);
+      });
+    }
+    // ======================================================================= ^ download more img ^ ============
     const lightbox = new SimpleLightbox('.gallery-list a', {
       captionsData: 'alt',
       captionDelay: 250,
     });
     lightbox.refresh();
   });
-
+  // ======================================================================= ^ librarys ^ ======================
   formSearchImg.reset();
 }
